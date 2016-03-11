73507557027545739edbe9a1bebe5eefc986252d
vonquilon <von.quilon@gmail.com>
2016-03-10 22:16:37 -0500

    implemented accesstoken validation

--- controllers/sessions.server.controller.js
+++ controllers/sessions.server.controller.js
@@ -1,54 +1,70 @@
 var Session = require('mongoose').model('Session'),
+	User = require('mongoose').model('User'),
 	errHandler = require('../utils/errHandler'),
 	strings = require('../utils/strings');
 
 exports.createSession = function(req, res) {
-	// TODO: use accesstoken
-
-	if (req.body.lastUsed) {
-		req.body.lastUsed = undefined;
-	}
-
-	var session = new Session(req.body);
-
-	session.save(function(err) {
+	User.findById(req.body.user, function(err, user) {
 		errHandler.handleErr(err, res, function() {
-			res.status(201).json(session.clean());
+			if (user && user.validateAccTok(req.get(strings.headerNames.accessToken))) {
+				if (req.body.lastUsed) {
+					req.body.lastUsed = undefined;
+				}
+
+				var session = new Session(req.body);
+
+				session.save(function(err) {
+					errHandler.handleErr(err, res, function() {
+						res.status(201).json(session.clean());
+					});
+				});
+			} else {
+				res.status(401).send(strings.statCode._401.unauthAcc);
+			}
 		});
 	});
 };
 
 exports.getSession = function(req, res) {
-	// TODO: use accesstoken
-
 	if (req.session) {
 		var session = req.session;
-		session.lastUsed = Date.now();
 
-		session.save(function (err) {
-			errHandler.handleErr(err, res, function() {
-				session.user.genAccTokAndSave({validateBeforeSave: false}, function(err) {
-					errHandler.handleErr(err, res, function() {
-						res.status(200).json(session.user.clean());
+		if (session.user.validateAccTok(req.get(strings.headerNames.accessToken))) {
+			session.lastUsed = Date.now();
+
+			session.save(function (err) {
+				errHandler.handleErr(err, res, function () {
+					session.user.genAccTokAndSave({validateBeforeSave: false}, function (err) {
+						errHandler.handleErr(err, res, function () {
+							res.status(200).json(session.user.clean());
+						});
 					});
 				});
 			});
-		});
+		} else {
+			res.status(401).send(strings.statCode._401.unauthAcc);
+		}
 	} else {
 		res.status(404).send(strings.statCode._404.sessionNotFound);
 	}
 };
 
-exports.deleteSession = function(req, res, next) {
-	// TODO: use accesstoken
+exports.deleteSession = function(req, res) {
+	if (req.session) {
+		var session = req.session;
 
-	req.session.remove(function(err) {
-		if (err) {
-			return next(err);
+		if (session.user.validateAccTok(req.get(strings.headerNames.accessToken))) {
+			session.remove(function(err) {
+				errHandler.handleErr(err, res, function() {
+					res.status(204).end();
+				});
+			});
+		} else {
+			res.status(401).send(strings.statCode._401.unauthAcc);
 		}
-
-		// TODO: res.send or res.json?
-	});
+	} else {
+		res.status(404).send(strings.statCode._404.sessionNotFound);
+	}
 };
 
 exports.findSessionById = function(req, res, next, id) {
