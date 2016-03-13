215053546ce4c51315111ec3d76342e2146a1a2b
vonquilon <von.quilon@gmail.com>
2016-03-13 17:00:40 -0400

    setting up the session model and api tests

--- tests/main/session.server.test.js
+++ tests/main/session.server.test.js
@@ -1,6 +1,24 @@
+var app = require('../../server'),
+    helper = require('../helpers/server.helper.js'),
+    should = require('should'),
+    request = require('supertest'),
+    errHandler = require('../../utils/errHandler'),
+    strings = require('../../utils/strings'),
+    Session = require('mongoose').model('Session');
+
 describe('Session Unit Tests:', function() {
 
     describe('Testing schema validations', function() {
 
+        var userId;
+
+        before(function(done) {
+            helper.getValidUser().genAccTokAndSave(function(user) {
+                userId = user._id;
+                done();
+            });
+        });
+
     }); // END--Testing schema validations
-});
+
+}); // END--Session Unit Tests:
