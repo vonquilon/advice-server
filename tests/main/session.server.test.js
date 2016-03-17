83cd9637f88df52ef8cb02ec72556fae41843673
vonquilon <von.quilon@gmail.com>
2016-03-16 22:06:20 -0400

    added schema tests for session model

--- tests/main/session.server.test.js
+++ tests/main/session.server.test.js
@@ -6,15 +6,50 @@ var app = require('../../server'),
     strings = require('../../utils/strings'),
     Session = require('mongoose').model('Session');
 
+// PUT ALL TESTS IN A SINGLE TEST FILE
+
 describe('Session Unit Tests:', function() {
 
     describe('Testing schema validations', function() {
 
-        var userId;
+        var user,
+            session;
 
         before(function(done) {
-            helper.getValidUser().genAccTokAndSave(function(user) {
-                userId = user._id;
+            user = helper.getValidUser();
+            user.save(function() {
+                done();
+            });
+        });
+
+        beforeEach(function() {
+            session = new Session({ user: user._id });
+        });
+
+        it('Should save without problems when all session fields are valid', function(done) {
+            session.save(function(err) {
+                should.not.exist(err);
+                done();
+            });
+        });
+
+        it('Should not save when the user ID is missing', function(done) {
+            session.user = undefined;
+            session.save(function(err) {
+                var errMsg = errHandler.getErrMsg(err);
+                errMsg.should.have.properties(helper.testErr('user is required'));
+                done();
+            });
+        });
+
+        afterEach(function(done) {
+            session.remove(function() {
+                done();
+            });
+        });
+
+        after(function(done) {
+            user.remove(function() {
                 done();
             });
         });
